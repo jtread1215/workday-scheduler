b83396ce6a079568f002f1b49b697426bb1b448a
Joshua Treadway <77247419+jtread1215@users.noreply.github.com>
2021-06-04 21:19:21 -0400

    updated html js and css

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,15 @@
-let today = $("#today");
+//Variables
+let today = $("#currentDay");
+let content = $("content");
+//let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+let timeSlot = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+let userStorage = window.localStorage;
 
 //Using moment.js to establish current day/time
-function showDate() {
-    let date = moment().format("dddd, MMMM Do, YYYY");
-    today.text(date);
-}
+today.text(moment().format('MMMM Do YYYY, h:mm a'));
 
-function showTime() {
-    let time = Number.parseInt(moment().format("H"));
-    today.text(time);
-}
 
-showDate();
-showTime();
-
+//Creating colors for time slots past/present/future
 
 
 
