da935130b0e51a8cf9dd2a5fc26cc799b505d5a5
Joshua Treadway <77247419+jtread1215@users.noreply.github.com>
2021-06-05 15:06:14 -0400

    fixed html and js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,53 @@
 //Variables
 let today = $("#currentDay");
-let content = $("content");
-//let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-let timeSlot = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
-let userStorage = window.localStorage;
+let locStorage = window.localStorage;
 
-//Using moment.js to establish current day/time
+//Using moment.js to establish current month/day/year/time
 today.text(moment().format('MMMM Do YYYY, h:mm a'));
 
+// Local storage data loaded if applicable
+$("#hour-9 .description").val(locStorage.getItem("hour-9"));
+$("#hour-10 .description").val(locStorage.getItem("hour-10"));
+$("#hour-11 .description").val(locStorage.getItem("hour-11"));
+$("#hour-12 .description").val(locStorage.getItem("hour-12"));
+$("#hour-13 .description").val(locStorage.getItem("hour-13"));
+$("#hour-14 .description").val(locStorage.getItem("hour-14"));
+$("#hour-15 .description").val(locStorage.getItem("hour-15"));
+$("#hour-16 .description").val(locStorage.getItem("hour-16"));
+$("#hour-17 .description").val(locStorage.getItem("hour-17"));
 
-//Creating colors for time slots past/present/future
+function update() {
+    //Using moment.js to establish current time then loop to determine past/current/future slots
+    let now = moment().hours();
+    $(".time-block").each(function() {
+        let slot = parseInt($(this).attr("id").split("-")[1]);
+    if ( slot < now) {
+        $(this).addClass("past");
+    }
+    else if ( slot === now) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+    });
+}
+
+update();
+
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        let entry = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+        locStorage.setItem(time, entry);
+    })
+})
+
+setInterval(update, 15000);
+
 
 
 
